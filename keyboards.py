1a560f210b0f73e7d2521e70abee0ef22606e657
Alexander <alex1995599105@gmail.com>
2026-04-23 21:43:36 +0200

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -22,3 +22,36 @@ def get_main_menu_keyboard():
         ]
     ]
     return InlineKeyboardMarkup(keyboard)
+
+
+def get_occasion_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "🎂 День рождения",
+                callback_data="birthday"
+            ),
+            InlineKeyboardButton(
+                "💍 Свадьба",
+                callback_data="wedding"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "🏫 В школу",
+                callback_data="school"
+            ),
+            InlineKeyboardButton(
+                "🌷 Без повода",
+                callback_data="no_reason"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "✏️ Другой повод",
+                callback_data="other_occasion"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
